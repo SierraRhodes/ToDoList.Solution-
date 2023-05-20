8b93471c455dd25853bee157ce0e926232bd349f
SierraRhodes <alreadywrote@gmail.com>
2023-05-19 20:30:46 -0700

    add edit and delete functionality

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -15,6 +15,30 @@ namespace HairSalon.Controllers
     {
       _db = db;
     }
+      public ActionResult Index()
+{
+  List<Client> model = _db.Clients.Include(Client => Client.Stylist).ToList(); 
+  ViewBag.PageTitle = "View All Clients";
+  return View(model);
+}
+
+    public ActionResult Create()
+{
+  ViewBag.Stylist.Id = new SelectList(_db.Stylists, "Stylist.Id", "Name");
+  return View();
+}
+
+    [HttpPost]
+    public ActionResult Create(Client Client)
+    {
+      if (Client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(Client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
     
   }
 }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using MySqlConnector;
 
 namespace HairSalon.Models
 {
